37edb2630c93b0dfcc0839f52157b82008bb4de8
Kevin BRET <kevin@canari.studio>
2024-03-25 14:30:31 +0100

    Page blog : mappage de tous les articles

---
+++ components/container/Container.js
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import { Children } from "react";
+
+export default function Container({children}) {
+  return (
+    <>
+      <nav className="navbar navbar-dark bg-primary">
+        <div className="container-fluid justify-content-center">
+          <Link href="/">
+            <span className="navbar-brand mx-4">Accueil</span>
+          </Link>
+          <Link href="/blog">
+            <span className="navbar-brand mx-4">Blog</span>
+          </Link>
+          <Link href="/utilisateur">
+            <span className="navbar-brand mx-4">Liste</span>
+          </Link>
+        </div>
+      </nav>
+
+      {children}
+    </>
+  );
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
-import "@/styles/globals.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Container from "../components/container/Container";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Container>
+      <Component {...pageProps} />
+    </Container>
+  );
 }
